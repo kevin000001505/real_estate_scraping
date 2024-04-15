38f669765a0eb5bdc6c59aa53dc0b5cd979a155f
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-04-15 20:31:55 +0800

    "main"

--- housing_scrapy拷貝/housing_scrapy/pipelines.py
+++ housing_scrapy拷貝/housing_scrapy/pipelines.py
@@ -10,6 +10,14 @@ from itemadapter import ItemAdapter
 
 class HousingScrapyPipeline:
     def process_item(self, item, spider):
+        if '年' in item['year']:
+            item['year'] = int(item['year'].replace("年", ""))
+
+        if '~' in item['public_equipment']:
+            parts = item['public_equipment'].replace("%", "").split('~')
+            item['public_equipment'] = (int(parts[0])+int(parts[1]))/2
+        if '%' in item['cover_percentage']:
+            item['cover_percentage'] = int(item['cover_percentage'].replace("%", ""))
         return item
 
     def store_in_db(self, item):
